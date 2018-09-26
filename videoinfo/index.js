376a9ae7fac86ae664cc133f86a5b3712bbc668e
A.Rupesh <a.rupesh66@gmail.com>
2018-09-27 02:46:46 +0530

    callbacks

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,9 +1,13 @@
 
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
    mainWindow.loadURL(`file://${__dirname}/index.html`);
+ 
+  ipcMain.on('video:submit', () => {
+  
 });
+
