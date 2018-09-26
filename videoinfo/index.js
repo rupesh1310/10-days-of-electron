11af8cc2aaead2363b5d0c05a79a189aee8c4e8e
A.Rupesh <a.rupesh66@gmail.com>
2018-09-27 01:22:32 +0530

    testing

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,1 +1,9 @@
 
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({});
+   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
