429de6ea144731563a4bbbccb421324893037e78
A.Rupesh <a.rupesh66@gmail.com>
2018-09-27 03:02:47 +0530

    bug fixed

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -4,6 +4,7 @@ const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
    mainWindow.loadURL(`file://${__dirname}/index.html`);
