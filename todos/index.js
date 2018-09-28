57573952dee60eb8d3f739124040f8265ab8bc08
A.Rupesh <a.rupesh66@gmail.com>
2018-09-29 01:47:06 +0530

    Update index.js

--- todos/index.js
+++ todos/index.js
@@ -3,12 +3,21 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
  mainWindow.loadURL(`file://${__dirname}/main.html`);
  
- const menuTemplate = [
+  function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo';
+  });
+}
+  
+  const menuTemplate = [
   {
     label: 'File',
     submenu: [
